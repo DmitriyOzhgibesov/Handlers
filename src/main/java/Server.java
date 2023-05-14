46eb57c3221d4c167173410fc22290f4af1a15d2
Dmitriy Ozhgibesov <dmitry.ozhghibesov@innowave.ai>
2023-05-15 02:32:23 +0700

    Clear code

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,12 +1,7 @@
-import javax.xml.crypto.dsig.keyinfo.KeyValue;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,7 +10,6 @@ public class Server {
     private ServerSocket serverSocket = null;
     Map<String, Handler> innerHandlersMap;
     Map<String, Map<String, Handler>> handlers;
-    List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     ExecutorService threadPool = Executors.newFixedThreadPool(64);
 
     public Server() {
@@ -41,71 +35,6 @@ public class Server {
         }
     }
 
-    /*
-        private void handleRequest(Socket socket) {
-            try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 final var out = new BufferedOutputStream(socket.getOutputStream());) {
-
-                final var requestLine = in.readLine();
-                System.out.println(requestLine);
-                System.out.println(Thread.currentThread().getName());
-                final var parts = requestLine.split(" ");
-
-                if (parts.length != 3) {
-
-                } else {
-                    final var path = parts[1];
-                    if (!validPaths.contains(path)) {
-                        out.write((
-                                "HTTP/1.1 404 Not Found\r\n" +
-                                        "Content-Length: 0\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.flush();
-                    } else {
-                        final var filePath = Path.of(".", "public", path);
-                        final var mimeType = Files.probeContentType(filePath);
-
-                        // special case for classic
-                        if (path.equals("/classic.html")) {
-                            handleClassic(out, filePath, mimeType);
-                        } else {
-                            final var length = Files.size(filePath);
-                            out.write((
-                                    "HTTP/1.1 200 OK\r\n" +
-                                            "Content-Type: " + mimeType + "\r\n" +
-                                            "Content-Length: " + length + "\r\n" +
-                                            "Connection: close\r\n" +
-                                            "\r\n"
-                            ).getBytes());
-                            Files.copy(filePath, out);
-                            out.flush();
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private void handleClassic(BufferedOutputStream out, Path filePath, String mimeType) throws IOException {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + content.length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            out.write(content);
-            out.flush();
-        }
-    */
     private void handleRequest(Socket socket) {
         try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              final var out = new BufferedOutputStream(socket.getOutputStream());) {
